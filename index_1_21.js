eea0ca8bf9ce52036e7445e3bc4e812fae850d05
Marcus Browne <marcusb08@gmail.com>
2024-01-23 18:14:35 -0500

    Review Session 1_23

--- index_1_21.js
+++ index_1_21.js
@@ -9,13 +9,7 @@
 // Given ab array of numbers and a given target. return the target if it's in the array or undefined if it's not
 function returnTarget(arr, target) {
 
-    // for (let i = 0; i < arr.length; i++) {
-    //     if (arr[i] === target) {
-    //         return target;
-    //     }
-    // }
-    // return undefined;
-    return arr.find(num => num === target);
+   return arr.find(num => num === target);
    
 }
 const myArray = [2,4,6,8,10];
@@ -26,39 +20,36 @@ console.log(result);
 
 // Given an array of numbers, return the first number that's bigger than 14 or undefined if no number is bigger than 14
 function findBiggerNumber(arr) {
-    // for (let num of arr) {
-    //     if (num > 14) {
-    //         return num;
-    //     }
-    // }
-    // return undefined;
-    return arr.find((num) => num > 14);
+  return arr.find(num => num > 14);
 
 }
 console.log(findBiggerNumber([9,12,5,7,8]));
-console.log(findBiggerNumber([9, 6, 3, 1]));
+console.log(findBiggerNumber([9, 6, 31, 1]));
 console.log(findBiggerNumber([1, 2, 4, 5, 14, 15]));
 
 
 // Given an array of strings, return the first string with length greater than 4 or undefined if no such string is found
 function findString(arr) {
-    return arr.find((str) => str.length > 4);
+ return arr.find(str => str.length > 4);
 
 
 }
-console.log(findString(["Hell", "Lotto", "Cat"]));
-console.log(findString(["Player", "Gametime", "Baller"]));
+console.log(findString(["Hello", "Lotto", "Cat"]));
+console.log(findString(["Play", "Gametime", "Baller"]));
 
 // Given an array of Objects and a target id return the first object that matches the id or undefined if no such object is found
 // Expected Structure of a SINGLE object - 
 
 function matchId(arr, targetId) {
-// for (let obj of arr) {
-//     if (obj.id === targetId) {
-//         return obj;
-//     }
-// }
-return arr.find(obj => obj.id === targetId);
+    // for (let obj of arr) {
+    //     if (obj.id === targetId) {
+    //         return obj;
+    //     }
+    // }
+
+    return arr.find(obj => obj.id === targetId);
+
+    
 }
 const newArray = [
     { "id": "Y42C_a", "name": "Robert", "occupation": "Chef", "eyeColor": "green" },
@@ -74,7 +65,7 @@ const newArray = [
   ];
   
   console.log(matchId(newArray,"R24G_e"));
-  console.log(matchId(newArray,"152r"));
+  console.log(matchId(newArray,"L79H_f"));
   console.log(matchId(newArray,"N47K_i"));
   
 /*
