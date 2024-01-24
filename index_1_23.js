bd9212e7667de63bac5a555e945cac43fbb12a7b
Marcus Browne <marcusb08@gmail.com>
2024-01-23 19:42:34 -0500

    Review Session solution 2

--- index_1_23.js
+++ index_1_23.js
@@ -1,24 +1,29 @@
 // 1.----------------------------------
-// Write a function that takes in a string and returns a new string with every word capitalized. Write your own examples to call the function with.
-function capitalizeWords(str) {
-    const words = str.split(" ");
-    // const capitalizedWords = []
+// // Write a function that takes in a string and returns a new string with every word capitalized. Write your own examples to call the function with.
+// function capitalizeWords(str) {
+//     const words = str.split(" ");
+//     // const capitalizedWords = []
     
   
- for (let i = 0; i < words.length; i++) {
-    const word = words[i];
-    words[i] = word[0].toUpperCase() + word.slice(1).toLowerCase();
-    // capitalizedWords.push(capitalWord);
-   }
- } 
+//  for (let i = 0; i < words.length; i++) {
+//     const word = words[i];
+//     words[i] = word[0].toUpperCase() + word.slice(1).toLowerCase();
+//     // capitalizedWords.push(capitalWord);
+//    }
+//  } 
 
 
 
-  // Example usage:
-// 2.----------------------------------
-// Write a function that takes in an object and returns a formatted greeting.
-// This is the format of the object - 
+//   // Example usage:
+// // 2.----------------------------------
+// // Write a function that takes in an object and returns a formatted greeting.
+// // This is the format of the object - 
 
+function formatGreeting (obj) {
+    return `Hello ${obj.firstName} ${obj.lastName}, have a great day at your job as a ${obj.occupation}`;
+
+}
+ 
 
 const john = {
     firstName: "John",
